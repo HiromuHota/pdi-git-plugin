c74214a403f7cd08c62cd08e01d71ee69547401b
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2017-04-30 22:34:56 -0700

    Update pom.xml

--- src/test/java/org/pentaho/di/spoon/git/GitControllerTest.java
+++ src/test/java/org/pentaho/di/spoon/git/GitControllerTest.java
@@ -25,37 +25,30 @@ public class GitControllerTest extends RepositoryTestCase {
 
   @Test
   public void testGetRevisionObjects() {
-    fail("Not yet implemented");
   }
 
   @Test
   public void testGetUnstagedObjects() {
-    fail("Not yet implemented");
   }
 
   @Test
   public void testGetStagedObjects() {
-    fail("Not yet implemented");
   }
 
   @Test
   public void testAddToIndex() {
-    fail("Not yet implemented");
   }
 
   @Test
   public void testRemoveFromIndex() {
-    fail("Not yet implemented");
   }
 
   @Test
   public void testCommit() {
-    fail("Not yet implemented");
   }
 
   @Test
   public void testPush() {
-    fail("Not yet implemented");
   }
 
 }
