4a615fb8fa23dd8217b74deddbe4edc6893f99c8
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2017-04-30 23:02:40 -0700

    Implement testGetRevisionObjects(), testGetUnstagedObjects(), testGetStagedObjects()

--- src/test/java/org/pentaho/di/spoon/git/GitControllerTest.java
+++ src/test/java/org/pentaho/di/spoon/git/GitControllerTest.java
@@ -2,19 +2,28 @@ package org.pentaho.di.spoon.git;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+
 import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.NoFilepatternException;
 import org.eclipse.jgit.junit.RepositoryTestCase;
 import org.junit.Before;
 import org.junit.Test;
+import org.pentaho.di.ui.repository.pur.repositoryexplorer.model.UIRepositoryObjectRevisions;
+import org.pentaho.di.ui.repository.repositoryexplorer.model.UIJob;
+import org.pentaho.di.ui.repository.repositoryexplorer.model.UIRepositoryObjects;
+import org.pentaho.di.ui.repository.repositoryexplorer.model.UITransformation;
 
 public class GitControllerTest extends RepositoryTestCase {
 
   private GitController controller = new GitController();
+  private Git git;
 
   @Before
   public void setUp() throws Exception {
     super.setUp();
-    Git git = new Git( db );
+    git = new Git( db );
     controller.setGit( git );
   }
 
@@ -24,15 +33,33 @@ public class GitControllerTest extends RepositoryTestCase {
   }
 
   @Test
-  public void testGetRevisionObjects() {
+  public void testGetRevisionObjects() throws IOException, NoFilepatternException, GitAPIException {
+    writeTrashFile( "Test.txt", "Hello world" );
+    git.add().addFilepattern( "Test.txt" ).call();
+    git.commit().setMessage( "initial commit" ).call();
+    UIRepositoryObjectRevisions revisions = controller.getRevisionObjects();
+    assertEquals( 1, revisions.size() );
   }
 
   @Test
-  public void testGetUnstagedObjects() {
+  public void testGetUnstagedObjects() throws Exception {
+    writeTrashFile( "a.ktr", "content" );
+    writeTrashFile( "b.kjb", "content" );
+    UIRepositoryObjects stagedObjects = controller.getUnstagedObjects();
+    assertEquals( 2, stagedObjects.size() );
+    assertEquals( UITransformation.class, stagedObjects.get( 0 ).getClass() );
+    assertEquals( UIJob.class, stagedObjects.get( 1 ).getClass() );
   }
 
   @Test
-  public void testGetStagedObjects() {
+  public void testGetStagedObjects() throws Exception {
+    writeTrashFile( "a.ktr", "content" );
+    writeTrashFile( "b.kjb", "content" );
+    git.add().addFilepattern( "." ).call();
+    UIRepositoryObjects stagedObjects = controller.getStagedObjects();
+    assertEquals( 2, stagedObjects.size() );
+    assertEquals( UITransformation.class, stagedObjects.get( 0 ).getClass() );
+    assertEquals( UIJob.class, stagedObjects.get( 1 ).getClass() );
   }
 
   @Test
