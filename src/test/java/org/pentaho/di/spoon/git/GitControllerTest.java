6041e37034994247144ba885e5b48a2504837312
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2017-04-30 21:15:40 -0700

    Add failing tests

---
+++ src/test/java/org/pentaho/di/spoon/git/GitControllerTest.java
@@ -0,0 +1,50 @@
+package org.pentaho.di.spoon.git;
+
+import static org.junit.Assert.*;
+
+import org.eclipse.jgit.junit.RepositoryTestCase;
+import org.junit.Test;
+
+public class GitControllerTest extends RepositoryTestCase {
+
+  @Test
+  public void testGetPath() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetRevisionObjects() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetUnstagedObjects() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetStagedObjects() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testAddToIndex() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testRemoveFromIndex() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testCommit() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testPush() {
+    fail("Not yet implemented");
+  }
+
+}
