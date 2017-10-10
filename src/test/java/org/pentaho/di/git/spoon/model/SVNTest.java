97d3e52f05360f09361efa90e3b66e622af7d4bb
Hiromu Hota <hiromu.hota@hal.hitachi.com>
2017-10-10 16:58:38 -0700

    Tests for SVN.diff,.open,.resetPath

--- src/test/java/org/pentaho/di/git/spoon/model/SVNTest.java
+++ src/test/java/org/pentaho/di/git/spoon/model/SVNTest.java
@@ -4,11 +4,15 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.StringWriter;
 import java.util.List;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.apache.subversion.javahl.ISVNRepos;
 import org.apache.subversion.javahl.SVNRepos;
+import org.eclipse.jgit.lib.Constants;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -54,6 +58,9 @@ public class SVNTest {
     revisions = vcs.getRevisions();
     assertEquals( VCS.WORKINGTREE, revisions.get( 0 ).getName() );
 
+    vcs.resetPath( "test.txt" );
+    vcs.add( "test.txt" );
+
     vcs.commit( "user", "message" );
     assertEquals( revisions.size(), vcs.getRevisions().size() );
     revisions = vcs.getRevisions();
@@ -61,6 +68,24 @@ public class SVNTest {
 
     List<UIFile> files = vcs.getStagedFiles( "0", "1" );
     assertEquals( "test.txt", files.get( 0 ).getName() );
+
+    InputStream inputStream = vcs.open( "test.txt", "1" );
+    StringWriter writer = new StringWriter();
+    IOUtils.copy( inputStream, writer, "UTF-8" );
+    assertEquals( "Hello World", writer.toString() );
+
+    inputStream = vcs.open( "test.txt", Constants.HEAD );
+    writer = new StringWriter();
+    IOUtils.copy( inputStream, writer, "UTF-8" );
+    assertEquals( "Hello World", writer.toString() );
+
+    inputStream = vcs.open( "test.txt", IVCS.WORKINGTREE );
+    writer = new StringWriter();
+    IOUtils.copy( inputStream, writer, "UTF-8" );
+    assertEquals( "Hello World", writer.toString() );
+
+    String diff = vcs.diff( "0", "1", "test.txt" );
+    assertTrue( diff.contains( "+Hello World" ) );
   }
 
   @Test
