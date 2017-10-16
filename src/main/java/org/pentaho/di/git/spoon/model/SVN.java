HiromuHota/pdi-git-plugin

commit c585207de81ffe1ef34e9c07ff787cd0803349eb

Refactor diff for when no commit exists

src/main/java/org/pentaho/di/git/spoon/model/SVN.java
@@ -28,10 +28,12 @@ import org.pentaho.di.ui.repository.pur.repositoryexplorer.model.UIRepositoryObj
 import org.pentaho.di.ui.repository.pur.repositoryexplorer.model.UIRepositoryObjectRevisions;
 import org.tigris.subversion.svnclientadapter.ISVNClientAdapter;
 import org.tigris.subversion.svnclientadapter.ISVNDirEntry;
+import org.tigris.subversion.svnclientadapter.ISVNInfo;
 import org.tigris.subversion.svnclientadapter.ISVNLogMessage;
 import org.tigris.subversion.svnclientadapter.ISVNStatus;
 import org.tigris.subversion.svnclientadapter.SVNClientAdapterFactory;
 import org.tigris.subversion.svnclientadapter.SVNClientException;
+import org.tigris.subversion.svnclientadapter.SVNInfoUnversioned;
 import org.tigris.subversion.svnclientadapter.SVNNodeKind;
 import org.tigris.subversion.svnclientadapter.SVNRevision;
 import org.tigris.subversion.svnclientadapter.SVNStatusKind;
@@ -120,19 +122,18 @@ public class SVN extends VCS implements IVCS {
     AbstractJhlClientAdapter client = (AbstractJhlClientAdapter) svnClient;
     OutputStream outStream = new ByteArrayOutputStream();
     try {
-      String target = null;
-      if ( oldCommitId.equals( Constants.HEAD ) ) {
-        target = directory + File.separator + FilenameUtils.separatorsToSystem( file );
-        if ( getRevisions().size() == 1 ) {
-          oldCommitId = null;
-        }
-      } else {
-        target = svnClient.getInfo( root ).getUrl().toString() + "/" + file;
+      String target = directory + File.separator + FilenameUtils.separatorsToSystem( file );
+      ISVNInfo info = svnClient.getInfoFromWorkingCopy( new File( target ) );
+      if ( info instanceof SVNInfoUnversioned ) {
+        return "Unversioned";
+      }
+      if ( info.getRevision() == null ) { // not commited yet
+        oldCommitId = null;
       }
       client.getSVNClient().diff(
           target,
           null, resolveRevision( oldCommitId ), resolveRevision( newCommitId ),
-          null, outStream, Depth.infinityOrImmediates( true ), null, true, false, false, false, false, false );
+          directory, outStream, Depth.infinityOrImmediates( true ), null, true, false, false, false, false, false );
       return outStream.toString().replaceAll( "\n", System.getProperty( "line.separator" ) );
     } catch ( Exception e ) {
       return e.getMessage();

src/test/java/org/pentaho/di/git/spoon/model/SVNTest.java
@@ -23,6 +23,7 @@ public class SVNTest {
   private ISVNRepos admin;
   private File rootServer;
   private File rootClient;
+  private String diff;
 
   @Before
   public void setUp() throws Exception {
@@ -54,6 +55,9 @@ public class SVNTest {
     File file = new File( rootClient.getPath(), "test.txt" );
     FileUtils.write( file, "Hello World" );
     vcs.add( "test.txt" );
+
+    diff = vcs.diff( Constants.HEAD, IVCS.WORKINGTREE, "test.txt" );
+    assertTrue( diff.contains( "nonexistent" ) );
     assertEquals( vcs.getRevisions().size(), revisions.size() + 1 );
     revisions = vcs.getRevisions();
     assertEquals( VCS.WORKINGTREE, revisions.get( 0 ).getName() );
@@ -84,7 +88,7 @@ public class SVNTest {
     IOUtils.copy( inputStream, writer, "UTF-8" );
     assertEquals( "Hello World", writer.toString() );
 
-    String diff = vcs.diff( "0", "1", "test.txt" );
+    diff = vcs.diff( "0", "1", "test.txt" );
     assertTrue( diff.contains( "+Hello World" ) );
   }
 

commit 68ddf21c824c51d7333e624ab0a4f31d3c7fcf1e

Restore diff for Windows. This is a regression from c585207d

src/main/java/org/pentaho/di/git/spoon/model/SVN.java
@@ -239,7 +239,7 @@ public class SVN extends VCS implements IVCS {
       client.getSVNClient().diff(
           target,
           null, resolveRevision( oldCommitId ), resolveRevision( newCommitId ),
-          directory, outStream, Depth.infinityOrImmediates( true ), null, true, false, false, true, false, false );
+          directory.replace( "\\", "/" ), outStream, Depth.infinityOrImmediates( true ), null, true, false, false, true, false, false );
       return outStream.toString().replaceAll( "\n", System.getProperty( "line.separator" ) );
     } catch ( Exception e ) {
       return e.getMessage();